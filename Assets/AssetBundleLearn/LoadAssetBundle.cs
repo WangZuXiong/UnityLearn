a70715a65e43f1462ffd7d201d45f6b92dd762fb
王祖雄 <wangzuxiong@galasports.net>
2020-01-14 14:21:48 +0800

    111

--- Assets/AssetBundleLearn/LoadAssetBundle.cs
+++ Assets/AssetBundleLearn/LoadAssetBundle.cs
@@ -2,118 +2,28 @@
 using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
+using System;
+
 
 public class LoadAssetBundle : MonoBehaviour
 {
-    [SerializeField]
-    private string _name1 = "cube";
-    [SerializeField]
-    private string _name2 = "cube1";
-    [SerializeField]
-    private string _path1 = "AssetBundleLearn/AssetBundles/cube";
-    [SerializeField]
-    private string _path2 = "AssetBundleLearn/AssetBundles/cube1";
-    [SerializeField]
-    private Transform _root;
-    private void Start()
+    private LoadAssetBundle()
     {
-        _path1 = Application.dataPath + "/" + _path1;
-        _path2 = Application.dataPath + "/" + _path2;
 
-        StartCoroutine(DownAB<GameObject>(_name1, _path1));
-        StartCoroutine(DownAB<GameObject>(_name2, _path2));
     }
 
-    /// <summary>
-    /// 远程下载
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="resName"></param>
-    /// <param name="url"></param>
-    /// <returns></returns>
-    private IEnumerator DownAB<T>(string resName, string url) where T : Object
+    private void Awake()
     {
-        UnityWebRequest unityWebRequest = UnityWebRequestAssetBundle.GetAssetBundle(url);
-        yield return unityWebRequest.SendWebRequest();
-        //AssetBundle assetBundle = DownloadHandlerAssetBundle.GetContent(unityWebRequest);
-        AssetBundle assetBundle = (unityWebRequest.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
-        Debug.LogError(assetBundle == null);
-        T gameObject = assetBundle.LoadAsset<T>(resName);
-        Instantiate(gameObject, _root);
-        assetBundle.Unload(false);
+        Instance = this;
     }
 
-    /// <summary>
-    /// WWW.LoadFromCacheOrDownload
-    /// </summary>
-    /// <returns></returns>
-    [System.Obsolete]
-    private IEnumerator LoadCacheOrDownloadFromFile()
-    {
-        while (Caching.ready == false)
-        {
-            yield return null;
-        }
-        WWW www = WWW.LoadFromCacheOrDownload(_path1, 1);
-        yield return www;
+    public static LoadAssetBundle Instance { get; private set; }
 
-        if (!string.IsNullOrEmpty(www.error))
-        {
-            Debug.Log(www.error);
-            yield break;
-        }
-        AssetBundle ab = www.assetBundle;
-        GameObject go = ab.LoadAsset<GameObject>(_name1);
-        Instantiate(go);
+    public void LoadAssetBundleAsync<T>(string resName, string url, Action<T> callBack) where T : UnityEngine.Object
+    {
+        StartCoroutine(LoadAssetbundleByUnityWebRequest(resName, url, callBack));
     }
 
-    /// <summary>
-    /// 二进制文件  同步加载
-    /// </summary>
-    private void LoadBinaryAB()
-    {
-        var binary = File.ReadAllBytes(_path1);
-        var ab = AssetBundle.LoadFromMemory(binary);
-        var go = ab.LoadAsset<GameObject>(_name1);
-        Instantiate(go);
-    }
-
-    /// <summary>
-    /// 二进制文件 异步加载
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator LoadBinaryABAsync()
-    {
-        var binary = File.ReadAllBytes(_path1);
-        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(binary);
-        yield return request;
-        AssetBundle ab = request.assetBundle;
-        GameObject cube = ab.LoadAsset<GameObject>(_name1);
-        Instantiate(cube);
-    }
-
-    /// <summary>
-    /// 从文件进行加载 同步
-    /// </summary>
-    private void LoadFileAB()
-    {
-        AssetBundle ab = AssetBundle.LoadFromFile(_path1);
-        GameObject go = ab.LoadAsset<GameObject>(_name1);
-        Instantiate(go);
-    }
-
-    /// <summary>
-    /// 从文件进行加载 异步
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator LoadFileABAsync()
-    {
-        AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync(_path1);
-        yield return request;
-        AssetBundle ab = request.assetBundle;
-        GameObject go = ab.LoadAsset<GameObject>(_name1);
-        Instantiate(go);
-    }
 
 
     /// <summary>
@@ -129,4 +39,192 @@ public class LoadAssetBundle : MonoBehaviour
             AssetBundle.LoadFromFile("AssetBundles/" + item);
         }
     }
+
+
+
+
+
+    #region 获取AssetBundle对象的常用API
+
+    /// <summary>
+    /// 远程下载
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="resName"></param>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    private IEnumerator LoadAssetbundleByUnityWebRequest<T>(string resName, string url, Action<T> callBack) where T : UnityEngine.Object
+    {
+        UnityWebRequest unityWebRequest = UnityWebRequestAssetBundle.GetAssetBundle(url);
+        yield return unityWebRequest.SendWebRequest();
+        //AssetBundle assetBundle = DownloadHandlerAssetBundle.GetContent(unityWebRequest);
+        AssetBundle assetBundle = (unityWebRequest.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
+
+        if (assetBundle == null)
+        {
+            throw new Exception("assetBundle = null");
+        }
+        T temp = assetBundle.LoadAsset<T>(resName);
+        callBack?.Invoke(temp);
+        assetBundle.Unload(false);
+    }
+
+    /// <summary>
+    /// WWW
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    private IEnumerator LoadAssetbundleByWWW(string path)
+    {
+        WWW www = new WWW(path);
+        yield return www;
+        AssetBundle assetBundle = www.assetBundle;
+        www.Dispose();
+    }
+
+    /// <summary>
+    /// WWW.LoadFromCacheOrDownload
+    /// </summary>
+    /// <returns></returns>
+    [System.Obsolete]
+    private IEnumerator LoadFromCacheOrDownload(string path)
+    {
+        while (Caching.ready == false)
+        {
+            yield return null;
+        }
+        WWW www = WWW.LoadFromCacheOrDownload(path, 1);
+        yield return www;
+
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+        AssetBundle ab = www.assetBundle;
+        www.Dispose();
+    }
+
+    /// <summary>
+    /// 从文件进行加载 
+    /// </summary>
+    /// <returns></returns>
+    private void LoadFromFile(string path)
+    {
+        AssetBundle ab = AssetBundle.LoadFromFile(path);
+    }
+
+    /// <summary>
+    /// 从文件进行加载 异步
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator LoadFromFileAsync(string path)
+    {
+        AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync(path);
+        yield return request;
+        AssetBundle ab = request.assetBundle;
+    }
+
+    /// <summary>
+    /// 二进制文件
+    /// </summary>
+    /// <param name="path"></param>
+    private void LoadFromMemory(string path)
+    {
+        var binary = File.ReadAllBytes(path);
+        AssetBundle ab = AssetBundle.LoadFromMemory(binary);
+    }
+
+    /// <summary>
+    /// 二进制文件 异步加载
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator LoadFromMemoryAsync(string path)
+    {
+        var binary = File.ReadAllBytes(path);
+        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(binary);
+        yield return request;
+        AssetBundle ab = request.assetBundle;
+    }
+
+    #endregion
+
+
+    #region 从AssetBundle加载资源的常用API
+
+    private void LoadAsset(AssetBundle assetBundle, string name)
+    {
+        object obj1 = assetBundle.LoadAsset(name);
+        object obj2 = assetBundle.LoadAsset(name, typeof(GameObject));
+
+    }
+
+    private IEnumerator LoadAssetAsync(AssetBundle assetBundle, string name)
+    {
+        object obj1 = assetBundle.LoadAssetAsync(name);
+        yield return obj1;
+
+        object obj2 = assetBundle.LoadAssetAsync(name, typeof(GameObject));
+        yield return obj2;
+    }
+
+    private void LoadAllAssets(AssetBundle assetBundle)
+    {
+        object[] objs1 = assetBundle.LoadAllAssets();
+        object[] objs2 = assetBundle.LoadAllAssets(typeof(GameObject));
+    }
+
+    private IEnumerator LoadAllAssetsAsync(AssetBundle assetBundle)
+    {
+        AssetBundleRequest obj1 = assetBundle.LoadAllAssetsAsync();
+        yield return obj1;
+        UnityEngine.Object[] temp = obj1.allAssets;
+        AssetBundleRequest obj2 = assetBundle.LoadAllAssetsAsync(typeof(GameObject));
+        yield return obj2;
+    }
+
+    #endregion
+
+
+    #region 卸载
+
+    /// <summary>
+    /// 卸载场景物件（GameObject）
+    /// </summary>
+    /// <param name="gameObject"></param>
+    private void UnloadGameObject(GameObject gameObject)
+    {
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// 卸载资源
+    /// </summary>
+    /// <param name="texture2D"></param>
+    private void UnloadAsset(Texture2D texture2D)
+    {
+        Resources.UnloadAsset(texture2D);
+    }
+
+    /// <summary>
+    /// 卸载资源
+    /// </summary>
+    private void UnloadUnusedAssets()
+    {
+        Resources.UnloadUnusedAssets();
+    }
+
+    /// <summary>
+    /// AssetBundle卸载
+    /// </summary>
+    /// <param name="assetBundle"></param>
+    private void UnLoadAssetbundle(AssetBundle assetBundle)
+    {
+        //卸载AssetBundle对象时保留内存中已加载的资源
+        assetBundle.Unload(false);
+        //卸载AssetBundle对象时卸载内存中已加载的资源，由于该方法容易引起资源引用丢失，因此并不建议经常使用
+        assetBundle.Unload(true);
+    }
+
+    #endregion
 }
