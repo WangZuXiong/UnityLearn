b80e553b3834f35ead6176364b2d3d5cddac9dfc
wangzuxiong <wangzuxiong@galasports.com>
2020-06-16 10:19:55 +0800

    lua error

--- Assets/Learn/XLuaLearn/LuaScript/LuaDispose.lua
+++ Assets/Learn/XLuaLearn/LuaScript/LuaDispose.lua
@@ -1,5 +1,10 @@
-﻿xlua.hotfix(CS.XLuaLoad, 'Func', nil)
+﻿local util = require 'xlua.util'
+util.print_func_ref_by_csharp()
+
+xlua.hotfix(CS.XLuaLoad, 'Func', nil)
 xlua.hotfix(CS.XLuaLoad, 'Play', nil)
 xlua.hotfix(CS.XLuaLoad, 'Play1', nil)
+xlua.hotfix(CS.XLuaLoad, 'Awake', nil)
+
 
 
