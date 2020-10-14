9c0dfe2859fee0940f21955c6c278671a8dc43fc
wangzuxiong <zuxiongwang@163.com>
2020-10-14 22:49:43 +0800

    1

--- Assets/_Lab/Test1.cs
+++ Assets/_Lab/Test1.cs
@@ -6,35 +6,48 @@ using UnityEngine.UI;
 [ExecuteInEditMode]
 public class Test1 : MonoBehaviour
 {
-    //[SerializeField]
-    //private Vector2 _pos;
+    [SerializeField]
+    private Vector2 _pos;
     [SerializeField]
     private Vector2 _size;
 
-    private void Start()
-    {
-
-    }
 
     private void Update()
     {
-        //Vector3 t = transform.TransformPoint(new Vector3(0, 0, 1));
-        //Debug.LogError(t);
-        //return;
         var t = Foo(Camera.main, GameObject.Find("Target"), _size);
-        //GetComponent<Image>().material.SetVector("_info", new Vector4(_pos.x, _pos.y, _size.x, _size.y));
         GetComponent<Image>().material.SetVector("_info", t);
     }
 
 
-    public Vector4 Foo(Camera camera, GameObject ui, Vector2 maskSize)
+    public Vector4 Foo(Camera camera, GameObject target, Vector2 offsetSize)
     {
-        var rectTrans = ui.GetComponent<RectTransform>();
-        var centerPos = new Vector2(
-            -(rectTrans.pivot.x - .5f) * rectTrans.rect.width * rectTrans.localScale.x,
-            -(rectTrans.pivot.y - .5f) * rectTrans.rect.height * rectTrans.localScale.y);
-        var worldPoint = ui.transform.TransformPoint(centerPos);
-        var screenPos = camera.WorldToScreenPoint(worldPoint);
-        return new Vector4(screenPos.x, screenPos.y, rectTrans.rect.width + maskSize.x, rectTrans.rect.height + maskSize.y);
+        var targetRect = target.GetComponent<RectTransform>();
+        var targetCenterPos = new Vector2(
+            -(targetRect.pivot.x - .5f) * targetRect.rect.width * targetRect.localScale.x,
+            -(targetRect.pivot.y - .5f) * targetRect.rect.height * targetRect.localScale.y);
+        var targetWorldPoint = target.transform.TransformPoint(targetCenterPos);
+
+        var imgRect = transform.GetComponent<RectTransform>();
+        var xScale = Screen.width / (float)(imgRect.rect.width * imgRect.localScale.x);
+        var yScale = Screen.height / (float)(imgRect.rect.height * imgRect.localScale.y);
+
+
+        var targetScreenPos = camera.WorldToScreenPoint(targetWorldPoint);
+        var imgCenterPos = new Vector2(
+            -(imgRect.pivot.x - .5f) * imgRect.rect.width * imgRect.localScale.x,
+            -(imgRect.pivot.y - .5f) * imgRect.rect.height * imgRect.localScale.y);
+        var imgLeftBottomPos = new Vector2(
+            imgCenterPos.x - 0.5f * imgRect.rect.width * imgRect.localScale.x,
+            imgCenterPos.y - 0.5f * imgRect.rect.height * imgRect.localScale.y);
+
+        var imgLeftBottomPosWorldPoint = transform.TransformPoint(imgLeftBottomPos);
+        var imgScreenPos = camera.WorldToScreenPoint(imgLeftBottomPosWorldPoint);
+
+
+        return new Vector4(
+            (targetScreenPos.x - imgScreenPos.x) * xScale,
+            (targetScreenPos.y - imgScreenPos.y) * yScale,
+            (targetRect.rect.width + offsetSize.x) * xScale,
+            (targetRect.rect.height + offsetSize.y) * yScale);
     }
 }
